ba957b0fbe19e8777adcc91a9aa1979418cb84ee
keko <adelquipompei@gmail.com>
2023-08-20 20:31:58 -0300

    movi de lugar la carpeta templates

--- tienda.js
+++ tienda.js
@@ -1,6 +1,6 @@
-import { indumentaria } from "./img/templates/indumentaria.js"
-import { efectoParallax } from "./img/templates/parallax.js"
-import { carrito, table, tbody, crearCaja } from "./img/templates/crearCaja.js"
+import { indumentaria } from "./templates/indumentaria.js"
+import { efectoParallax } from "./templates/parallax.js"
+import { carrito, table, tbody, crearCaja } from "./templates/crearCaja.js"
 
 efectoParallax()
 crearCaja(indumentaria)
