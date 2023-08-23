9f9bca21f6db897d14fbbf5a63b6fae25f2d680e
keko <adelquipompei@gmail.com>
2023-08-23 00:55:37 -0300

    .

--- templates/hoverDelete.js
+++ templates/hoverDelete.js
@@ -9,29 +9,29 @@ let svgDelete = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler
 
 const hoverDelete = (tableRow, td3, carrito,acum, changuito, total) => {
   td3.classList = 'btn-delete';
-  
   let btnTrash = tableRow.querySelector('.btn-delete');
-
   btnTrash.addEventListener('click', () => {
     // Encuentra el índice de la fila dentro de la tabla
     const indiceFila = Array.from(tableRow.parentNode.children).indexOf(tableRow);
-    
-
     if (indiceFila !== -1) {
       // Elimina el elemento correspondiente del carrito
-
       carrito.splice(indiceFila, 1);
-
       const carritoEnLocalStorage = JSON.parse(localStorage.getItem('carrito'));
       carritoEnLocalStorage.splice(indiceFila, 1);
       localStorage.setItem('carrito', JSON.stringify(carritoEnLocalStorage));
+      tableRow.remove();
+      carrito.forEach(producto => {
+      acum = acum + producto.precio
+      })
+  
+
+    
+
+
+
 
       // Elimina la fila de la tabla
-      tableRow.remove();
      //acum = 0
-     carrito.forEach(producto => {
-      acum = acum + producto.precio
-    })
     total.textContent = 'Total:  ' + acum.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' });
     changuito.innerHTML = carritoEnLocalStorage.length
 
